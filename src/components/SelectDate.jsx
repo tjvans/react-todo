2a965774d0e68441d114c54e6934e84c0881ee3e
tjvans <tjvans@users.noreply.github.com>
2023-11-28 09:48:23 +0000

    Apply formating changes

--- src/components/SelectDate.jsx
+++ src/components/SelectDate.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 
-export function SelectDate () {
-    const [value, onChange] = useState(new Date())
+export function SelectDate() {
+  const [value, onChange] = useState(new Date());
 
-    return (
-        <div className="selectdate-container">
-            <Calendar onChange={onChange} value={value} />
-        </div>
-    )
+  return (
+    <div className="selectdate-container">
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
 }
-
