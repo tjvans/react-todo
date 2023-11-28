699316df54603c4f1bb2809259aa7ca61febfe47
tjvans <tjvans@users.noreply.github.com>
2023-11-28 11:13:19 +0000

    Apply formating changes

--- src/components/TimeDate.jsx
+++ src/components/TimeDate.jsx
@@ -2,24 +2,24 @@ import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 
 export default function TimeDate() {
-  const [status, setStatus] = useState("display")
-    const [selectedDate, setSelectedDate] = useState(new Date());
-  const [time, setTime] = useState(() => new Date())
+  const [status, setStatus] = useState("display");
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [time, setTime] = useState(() => new Date());
 
-    function useTime () {
-        useEffect(() => {
-            const id = setInterval(() => {
-                setTime(new Date())
-                }, 1000)
-            return () => clearInterval(id)
-        }, [])
-        return time;
-    }
-    const currentTime = useTime()
+  function useTime() {
+    useEffect(() => {
+      const id = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+      return () => clearInterval(id);
+    }, []);
+    return time;
+  }
+  const currentTime = useTime();
 
   return (
     <div className="selectdate-container">
-       {time.toLocaleTimeString()}
+      {time.toLocaleTimeString()}
       <Calendar onChange={setSelectedDate} value={selectedDate} />
     </div>
   );
